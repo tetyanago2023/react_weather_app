209e08610a93786592e875f3493a7af227248808
go2023 <tetyanago2018@gmail.com>
2024-02-25 17:21:02 -0800

    Search and Weather components were created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Weather from "./components/weather";
 
 function App() {
   return (
     <div className="App">
+      <Weather />
     </div>
   );
 }
---
+++ src/components/weather/index.jsx
@@ -0,0 +1,10 @@
+const Weather = () => {
+
+    return (
+        <div>
+        <h1>Weather</h1>
+        </div>
+    );
+};
+
+export default Weather;
