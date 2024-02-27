94904884df27dabb994340cc140a073759e5b36c
go2023 <tetyanago2018@gmail.com>
2024-02-26 19:29:08 -0800

    Weather component was briefly created

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -5,13 +5,12 @@ const Search = ({search, setSearch, handleSearch}) => {
         <div className="search-engine">
             <input
                 type="text"
-                className="city-search"
                 placeholder="Enter City Name"
                 name="search"
                 value={search}
                 onChange={(event) => setSearch(event.target.value)}
             />
-            <button className="search-btn" onClick={handleSearch}>Search Weather</button>
+            <button onClick={handleSearch}>Search Weather</button>
         </div>
     );
 };
--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,10 +1,43 @@
 import Search from "../search";
+import {useState} from "react";
 
 const Weather = () => {
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setWeatherData] = useState(null);
+
+    async function fetchWeatherData(param) {
+        setLoading(true);
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=1c1de40153423aaa0bc39f72145a849c`
+            );
+
+            const data = await response.json();
+            if (data) {
+                setWeatherData(data);
+                setLoading(false);
+            }
+        } catch (e) {
+            setLoading(false);
+            console.log(e);
+        }
+    }
+
+    async function handleSearch() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=YOUR_API_KEY`);
+        const data = await response.json();
+        console.log(data);
+
+    }
 
     return (
         <div>
-            <Search />
+            <Search
+                search={search}
+                setSearch={setSearch}
+                handleSearch={handleSearch}
+            />
             <h2>Weather</h2>
 
         </div>
