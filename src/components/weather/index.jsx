97b7f216f6c2e04fc27b608dea09a8d5860c532b
go2023 <tetyanago2018@gmail.com>
2024-02-26 23:42:57 -0800

    Weather app functionality was implemented

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -6,23 +6,23 @@ const Weather = () => {
     const [loading, setLoading] = useState(false);
     const [weatherData, setWeatherData] = useState(null);
 
-    async function fetchWeatherData(param) {
+    const fetchWeatherData = async (param) => {
         setLoading(true);
         try {
             const response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=1c1de40153423aaa0bc39f72145a849c`
+                `https://api.openweathermap.org/data/2.5/weather?q=${param}&units=metric&appid=1c1de40153423aaa0bc39f72145a849c`
             );
             const data = await response.json();
-            console.log(data, "data");
             if (data) {
-                setWeatherData(data,);
+                setWeatherData(data);
                 setLoading(false);
             }
         } catch (e) {
             setLoading(false);
             console.log(e);
         }
-    }
+    };
+
 
     const handleSearch = () => {
         fetchWeatherData(search);
@@ -40,6 +40,28 @@ const Weather = () => {
     useEffect(() => {
         fetchWeatherData("London");
     }, []);
+
+    if (!weatherData) {
+        return <div>Loading...</div>;
+    }
+
+    // Round the temperature to 1 decimal place
+    const roundedTemperature = weatherData.main.temp.toFixed(0);
+    const roundedWindSpeed = weatherData.wind.speed.toFixed(0);
+
+    // Check if weatherData and weather description are available
+    const description =
+        weatherData &&
+        weatherData.weather &&
+        weatherData.weather[0] &&
+        weatherData.weather[0].description
+            ? weatherData.weather[0].description
+            : "";
+
+    // Capitalize the first letter of the description
+    const capitalizedDescription =
+        description.charAt(0).toUpperCase() + description.slice(1);
+
 
     console.log(weatherData, "weatherData");
 
@@ -59,6 +81,27 @@ const Weather = () => {
                     </div>
                     <div className="date">
                         <span>{getCurrentDate()}</span>
+                    </div>
+                    <div className="temp">
+                        <p>Temperature</p>
+                        {roundedTemperature} °C
+                    </div>
+                    <p className="description">
+                        {capitalizedDescription}
+                    </p>
+                    <div className="weather-info">
+                        <div className="column">
+                            <div>
+                                <p>Wind Speed</p>
+                                <p className="wind">{roundedWindSpeed} m/c</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div>
+                                <p>Humidity</p>
+                                <p className="humidity">{weatherData?.main?.humidity}%</p>
+                            </div>
+                        </div>
                     </div>
                 </div>}
 
